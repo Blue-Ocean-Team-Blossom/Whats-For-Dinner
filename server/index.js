adc24a2a9c40a078a9a55aae2b476ef5efde9658
kwakster4 <kimbyung4@utexas.edu>
2021-05-24 12:44:05 -0500

    chaged the ingredient autocomplete

--- server/index.js
+++ server/index.js
@@ -89,19 +89,24 @@ app.delete('/pantry', (req, res)=>{
 app.get('/ingredients', (req, res) => {
   // console.log(req.query);
   let {query} = req.query;
-  console.log(query);
 
+  // console.log(`${process.env.RAPID_API_URL}/food/ingredients/search`);
   axios({
     method: 'get',
-    url: `${process.env.RAPID_API_URL}/food/ingredients/search`,
+    url: `${process.env.RAPID_API_URL}/food/ingredients/autocomplete`,
     params: {
       query: query,
+      metaInformation: true,
       apiKey: process.env.API_KEY
+    },
+    headers: {
+      'x-rapidapi-key': process.env.RAPID_API_KEY,
+      'x-rapidapi-host': process.env.RAPID_API_HOST
     }
   })
   .then((results) => {
-    res.json(results.data.results);
-    // console.log(results.data.results);
+    res.json(results.data);
+    console.log(results.data);
   })
   .catch((err) => {
     console.error(err);

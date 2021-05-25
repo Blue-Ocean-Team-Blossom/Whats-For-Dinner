a2669f5ea3eeacebafcc47e0a2c8c00e16ce5557
kwakster4 <kimbyung4@utexas.edu>
2021-05-24 19:17:21 -0500

    implemented controller functions for favorites

--- database/controller.js
+++ database/controller.js
@@ -11,17 +11,33 @@ const deletePantry = (id) => {
     }
   });
 };
-
 const getPantry = (id) => {
   return Pantry.findAll({
     where: {
       userId: id
     }
   });
-}
-
+};
 const updatePantry = (pantryId, updateObj) => {
   return Pantry.update(updateObj, {where: {id: pantryId}});
-}
+};
+
+const postFavorite = (favObj) => {
+  return Favorite.create({...favObj});
+};
+const deleteFavorite = (id) => {
+  return Favorite.destroy({
+    where: {
+      id: id
+    }
+  });
+};
+const getFavorite = (id) => {
+  return Favorite.findAll({
+    where: {
+      userId: id
+    }
+  });
+};
 
 module.exports = {postPantry, updatePantry, getPantry, deletePantry};
